7651aad805eb8e94e0d1f3ff81d9648803d954b1
JangHyeonHo <46519658+JangHyeonHo@users.noreply.github.com>
2020-03-12 14:12:37 +0900

    등록 버튼 활성화
    커밋

--- springproject/src/main/webapp/resources/js/material/material_main.js
+++ springproject/src/main/webapp/resources/js/material/material_main.js
@@ -2,6 +2,18 @@
  * 재료 메인 관련 js
  */
 $(function(){
-
 	
+	/**
+ 	* 재료 등록 버튼 클릭시 팝업창 호출
+ 	*/
+	$("#registBtn").on("click",function(){
+		window.open('./material/regist'
+			    ,null
+			    ,'width=400
+			    ,height=500
+			    ,toolbar=no
+			    ,scrollbars=no
+			    ,menubar=no
+			    ,resizable=no');
+	})
 })
