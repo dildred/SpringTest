a459bf75deb161baa838fe6243b5ca68b5633b71
JangHyeonHo <46519658+JangHyeonHo@users.noreply.github.com>
2020-03-16 10:24:49 +0900

    여러가지 수정
    팝업관련 수정

--- springproject/src/main/webapp/resources/js/material/material_main.js
+++ springproject/src/main/webapp/resources/js/material/material_main.js
@@ -6,14 +6,43 @@ $(function(){
 	/**
  	* 재료 등록 버튼 클릭시 팝업창 호출
  	*/
-	$("#registBtn").on("click",function(){
-		window.open('./material/regist'
+	var isRegistEnd = false;
+	$("#registBtn").on("click",registPopUp());
+
+	function registPopUp(){
+		isRegistEnd = false;
+		var regPopup = window.open('./material/regist'
 			    ,null
-			    ,'width=400
-			    ,height=500
-			    ,toolbar=no
-			    ,scrollbars=no
-			    ,menubar=no
-			    ,resizable=no');
-	})
+			    ,'width=400,height=500,toolbar=no,scrollbars=no,menubar=no,resizable=no');
+		
+		var interval = window.setInterval(function() {
+			try {
+			    if (regPopup == null || regPopup.closed) {
+				window.clearInterval(interval);
+				isRegistCheck();
+			    }
+			}
+			catch (e) {
+			}
+		    }, 1000);
+	}
+	/**
+ 	* 재료 팝업창 종료시 이벤트(등록을 해서 종료가 된 것인지 혹은 그냥 종료한 것인지)
+ 	*/
+	function isRegistCheck(){
+		if(isRegistEnd){
+			alert("정상적으로 등록이 완료되었습니다");
+			if(confirm("이어서 등록하시겠습니까?")){
+				registPopUp();
+			} else {
+				isRegistEnd = false;
+			}
+		}
+	}
+	/**
+ 	* 재료 팝업창 종료의 정상 종료 여부 
+ 	*/
+	function setRegistEnd(bool){
+		isRegistEnd = bool;
+	}
 })
